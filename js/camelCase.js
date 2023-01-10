1d68ca3215389368fb28c8194b7b33bfef76e0f4
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2023-01-10 18:17:32 +0200

    start

--- js/camelCase.js
+++
@@ -1,3 +0,0 @@
-const authorizationToken = "a";
-
-const KEY = "1uutuyuasd2sd";
--- js/logicalOperator.js
+++
@@ -1,5 +0,0 @@
-const age = asd;
-const register = false;
-
-console.log("object :>> ", age >= 18 && register === true);
-console.log("object :>> ", age >= 17 || register === true);
