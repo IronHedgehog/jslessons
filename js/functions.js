c42bae2cb870729760d4a9ad40e44123dc054225
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2023-01-31 18:23:30 +0200

    start

--- js/functions.js
+++ js/functions.js
@@ -1,42 +1,93 @@
-function spliceArray(array) {
-  const start = array.indexOf("JavaScript");
-  const arr = array.splice(start, 3);
-  return arr;
-}
+// 1)Створити об'єкт, який буде описувати вас, створити не менше 5 ключів, вставити
+// туди декілька функції з ваших повсякденних справ ( наприклад виконання домашки та прогулянка)
+// за аналогією книжкової шафи яку ми з вами робили на уроці
 
-const secondPart = spliceArray(["I", "study", "JavaScript", "right", "now"]);
+// 2)Напишіть функцію, яка приймає як аргумент відстань в кілометрах і повертає відстань в милях.
+// Формула для перетворення : милі = кілометри * 0.6214.
 
-function sliceArray(array) {
-  const arr = array.slice(0, 2);
-  return arr;
-}
+// 3)Створіть функцію spliceArr(array),що приймає масив,
+// знаходить індекс 2-го елементу у масиві ["Apple","Orange","Plum",]
+// за допомогою методу масива та запишіть їй у змінну старт,
+// потім використайте цю змінну для заміни цього фрукта на "grapefruit"
 
-const firstPart = sliceArray(["I", "study", "JavaScript", "right", "now"]);
+//
 
-console.log(firstPart);
+// 1)Напиши функцію includes(array, value), яка робить те саме,
+// що і метод масиву масив.includes(значення) - перевіряє,
+// чи є в масиві array значення value, повертаючи true якщо є і false в іншому випадку.
 
-function сoncatArr(array1, array2) {
-  const arr = array1.concat(array2);
+// При виконанні цього завдання у тілі функції includes()
+// не можна використовувати метод масив.includes(значення).
 
-  const a = arr.join(" ");
-  return a;
-}
+// 2) function findNumber(start, end, divisor) {
+//   let number;
 
-const message = сoncatArr(firstPart, secondPart);
+//   for (let i = start; i < end; i += 1) {
+//     if (i % divisor === 0) {
+//       number += i
+//       break
+//     }
+//   }
+//   return number;
+// }
 
-console.log("message :>> ", message);
+// Виконай рефакторинг функції findNumber(start, end, divisor) так, щоб вона:
 
-// 1)Cтворити функцію  spliceArray яка приймає (масив) таким чином, щоб вона повертала у змінній arr  останні 3
-// елементи з масиву ["I", "study", "JavaScript", "right", "now"];
+// повертала перше число від start до end, яке ділиться на divisor без залишку
+// не використала оператор break
+// не використовувала змінну number
 
-//  2)Створити функцію  sliceArray(масив) так, щоб вона
-//  повертала перші два елементи з цього масиву ["I", "study", "JavaScript", "right", "now"]
-// (slice(start, end) – створює новий масив, копіюючи в нього елементи з позиції start до end (не включаючи end).
-// Приклад:
-// let arr = ["t", "e", "s", "t"];
+// 3)const start = 6;
+// const end = 27;
+// let number;
 
-// alert( arr.slice(1, 3) ); // e,s (копіює з 1 до 3)
-// )
+// for (let i = start; i < end; i += 1) {
+//   if (i % 5 === 0) {
+//     number = i;
+//   }
+// }
 
-//  3)Функція сoncatArr(array,array) має поєднувати два масиви які у вас вийшли з
-// двох минулих функцій таким чином, щоб у вас вийшов знову цілий масив ["I", "study", "JavaScript", "right", "now"];
+// Доповни код так, щоб у змінну number записувалося перше число від start до end, яке ділиться на 5 без залишку.
+
+// 4)function calculateTotalPrice(order) {
+//   let total = 0;
+
+//   for (let i = 0; i < order.length; i += 1) {
+//     total += order[i];
+//   }
+
+//   return total;
+// }
+
+// console.log('calculateTotalPrice([12, 85, 37, 4]) :>> ', calculateTotalPrice([12, 85, 37, 4]));
+
+// Виконай рефакторинг коду функції calculateTotalPrice(order) замінивши цикл for на for...of
+
+// 5)function filterArray(numbers, value) {
+//   const filteredNumbers = [];
+
+//   for (let i = 0; i < numbers.length; i += 1) {
+//     const number = numbers[i];
+
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   }
+
+//   return filteredNumbers;
+// }
+
+// console.log('filterArray([1, 2, 3, 4, 5], 3) :>> ', filterArray([1, 2, 3, 4, 5], 3));
+// виконай рефакторинг функції filter Array(numbers, value) замінивши цикл for на for...of.
+
+// function calculateTotalPrice(order) {
+//   let total = 0;
+
+//   return total;
+// }
+
+// calculateTotalPrice([12, 85, 37, 4])
+
+// Напиши функцію calculateTotalPrice(order),
+// яка приймає один параметр order - масив чисел і розраховує загальну суму його елементів.
+//  Загальна сума елементів повинна зберігатись у змінній total, яка повертається, як результат роботи функції.
