18c94bbf24a6107735902741a51dbfd1de393e1a
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2023-02-07 08:44:57 +0200

    s

---
+++ js/homework.js
@@ -0,0 +1,67 @@
+const workers = [
+  {
+    name: "bill",
+    job: "architect",
+    salary: 10000,
+  },
+  {
+    name: "Arni",
+    job: "actor",
+    salary: 100000,
+  },
+  {
+    name: "Bob",
+    job: "singer",
+    salary: 40000,
+  },
+  {
+    name: "Ann",
+    job: "manager",
+    salary: 30000,
+  },
+  {
+    name: "Antony",
+    job: "Footballer",
+    salary: 90000,
+  },
+  {
+    name: "John",
+    job: "builder",
+    salary: 25000,
+  },
+  {
+    name: "Arthur",
+    job: "teacher",
+    salary: 20000,
+  },
+];
+
+//1) створити функцію яка порахує середню зарплатню робітників.
+
+//
+
+const workers1 = [
+  {
+    name: "Robert",
+    job: "Dentist",
+    salary: 90000,
+  },
+  {
+    name: "Pietro",
+    job: "Computer Systems Analyst",
+    salary: 120000,
+  },
+  {
+    name: "James",
+    job: "Web Developer",
+    salary: 100000,
+  },
+];
+
+// 2) за допомогою синтаксису трьох крапок(...spread) поєднати масиви workers та workers1 та вивести у консоль
+
+//
+
+const arr = [1, 2, 3, 4, 5, 6];
+
+//3) виконати деструктуризацію масиву arr та вивести отримані константи у консоль.
