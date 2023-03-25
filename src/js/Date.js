bf0ec28d062cb3baa2d8238484784432aa3f76fc
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2023-03-25 14:17:17 +0200

    Q&A

--- src/js/Date.js
+++ src/js/Date.js
@@ -1,14 +1,7 @@
-const date = new Date();
-// console.log('date :>> ', date.getFullYear());
-// console.log('date :>> ', date.getDay());
-// console.log('date :>> ', date.getHours());
-// console.log('date :>> ', date.getDate());
-console.log('date :>> ', date.getMonth());
+import * as dayjs from 'dayjs';
+import * as localizedFormat from 'dayjs/plugin/localizedFormat';
 
-const nextMeet = new Date('March 25 2023 13:00:00');
-console.log(nextMeet);
+dayjs.extend(localizedFormat);
+const dayJSObj = dayjs();
 
-console.log(date.getUTCDate());
-console.log(date.getUTCFullYear());
-console.log(date.getUTCMilliseconds());
-// console.log(date.());
+console.log(dayJSObj.format('L'));
