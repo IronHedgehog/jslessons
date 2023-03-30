f4c5acea051a8bf0d3597f911b4e8d04c7e555ff
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2023-03-30 20:45:17 +0300

    promis

--- src/js/promis.js
+++ src/js/promis.js
@@ -1,16 +1,41 @@
-const value = false;
+const { default: axios } = require('axios');
+
+const yep = user => {
+  return (promisee = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() < 0.5) {
+        resolve(`${user} отримує шоколадка`);
+      } else {
+        reject('отримали оцінку нижче 8');
+      }
+    }, 2000);
+  }));
+};
 
 const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    if (value) {
-      resolve('Відповідь є');
+    if (Math.random() < 0.5) {
+      resolve('шоколадка');
     } else {
-      reject('Відповіді нема');
+      reject('отримали оцінку нижче 8');
     }
-  }, 1000);
-});
+  }, 2000);
+})
+  .then(item => console.log(item))
+  .catch(err => console.log(err))
+  .finally(() => console.log('проміси гуд'));
 
-promise
-  .then(value => console.log('value :>> ', value))
-  .catch(err => console.log('err :>> ', err))
-  .finally(history.push(index.html));
+yep('user')
+  .then(user => console.log(user))
+  .catch(err => console.log(err));
+
+const func = async () => {
+  try {
+    const response = await axios.get(
+      'https://jsonplaceholder.typicode.com/posts'
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
