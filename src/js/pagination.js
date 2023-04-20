b2e861e5772e904d2fd3ebc8bed27ea026a928ad
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2023-04-20 20:35:55 +0300

    pag

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,17 +1,69 @@
-import axios from 'axios';
+const { default: axios } = require('axios');
+// const { debounce } = require('debounce');
+// import Notiflix from 'notiflix';
 
-const params = new URLSearchParams({
-  _limit: 20,
-  _page: 5,
-  per_page: 40,
-});
+const refs = {
+  postDiv: document.querySelector('.posts'),
+  // q: document.querySelector('.q'),
+  loadMore: document.querySelector('.button'),
+};
 
-const page = 10;
+const B_URL = 'https://jsonplaceholder.typicode.com/';
+const currentResource = 'posts';
 
-const posts = fetch(`https://pixabay.com/api?${params}`)
-  // key=2313214498&q=adasd&
-  .then(response => response.json())
-  .then(json => console.log(json));
+let dataLimit = 10;
+let page = 1;
 
-const pag = 500 / 20;
-console.log(pag);
+async function getData(page = 1, limit = 10) {
+  // q = 'car'
+  const response = await axios.get(
+    `${B_URL}${currentResource}?&_page=${page}&_limit=${limit}`
+    // q=${q}
+  );
+  const data = await response.data;
+  return data;
+}
+
+async function postMarkup(data) {
+  const markUp = data
+    .map(({ title }) => {
+      return `<div>${title} </div>`;
+    })
+    .join(' ');
+  refs.postDiv.insertAdjacentHTML('beforeend', markUp);
+}
+
+window.addEventListener('load', onLoad);
+
+async function onLoad(e) {
+  const data = await getData();
+  postMarkup(data);
+}
+
+refs.loadMore.addEventListener('click', onClick);
+
+async function onClick(e) {
+  const pageLimit = 110 / dataLimit;
+  page += 1;
+  if (page <= pageLimit) {
+    refs.loadMore.style.display = 'block';
+    const data = await getData(page);
+    postMarkup(data);
+  } else {
+    refs.loadMore.style.display = 'none';
+  }
+}
+
+// refs.q.addEventListener('input', debounce(onInput, 300));
+
+// async function onInput(e) {
+//   const value = e.target.value.trim();
+
+//   if (!value) {
+//     Notiflix.Notify.info('Введіть тему');
+//     return;
+//   }
+
+//   const data = await getData(value);
+//   postMarkup(data);
+// }
