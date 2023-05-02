eb70fc24233abe9d181c6575fb136e58e270fed4
IronHedgehog <91615835+IronHedgehog@users.noreply.github.com>
2023-05-02 20:50:55 +0300

    CRUD -CR

--- src/js/crud.js
+++ src/js/crud.js
@@ -1,37 +1,76 @@
-// // CREATE - POST
+// CRUD
 
-// const { default: axios } = require('axios');
+// С - create - Створити - метод POST
+// R - read - прочитатит або отримати - метод GET
+// U - update - оновити данні - Якщо оновити об'єкт повністю то PUT, якщо оновити точечно (одне поле) то PATCH
+// D - delete - Видалити - метод DELETE
 
-// const user = {
-//   email:
+import Notiflix from 'notiflix';
 
-// };
+const refs = {
+  todoDiv: document.querySelector('#todos'),
+  addButton: document.querySelector('#add'),
+  input: document.querySelector('#create-input'),
+};
 
-// fetch('https://jsonplaceholder.typicode.com/posts', {
-//   method: 'POST',
-//   body: JSON.stringify(user),
-//   headers: {
-//     'Content-type': 'application/json; charset=UTF-8',
-//   },
-// })
-//   .then(response => response.json())
-//   .then(json => console.log(json));
+async function getTodos() {
+  const data = await fetch(
+    'https://jsonplaceholder.typicode.com/todos?_limit=10'
+  );
+  const todos = await data.json();
+  console.log(todos);
+  todos.forEach(todo => createHTML(todo));
+}
 
-// axios.post('https://jsonplaceholder.typicode.com/posts', user);
+window.addEventListener('DOMContentLoaded', getTodos());
 
-// // PUT - оновлення
+function createHTML({ completed, id, title }) {
+  console.log(completed);
+  const markup = `<div class="form-check" id="todo-${id}">
+  <label class="form-check-label">
+    <input type="checkbox" class="form-check-input" checked="${completed}"/>
+   ${title}
+  </label>
+  <button
+    type="button"
+    class="btn-close"
+    aria-label="Close"
+    style="font-size: 10px"
+  ></button>
+</div>`;
+  refs.todoDiv.insertAdjacentHTML('beforeend', markup);
+}
 
-// fetch('https://jsonplaceholder.typicode.com/posts/1', {
-//   method: 'PUT',
-//   body: JSON.stringify({
-//     id: 1,
-//     title: 'foo',
-//     body: 'bar',
-//     userId: 1,
-//   }),
-//   headers: {
-//     'Content-type': 'application/json; charset=UTF-8',
-//   },
-// })
-//   .then(response => response.json())
-//   .then(json => console.log(json));
+refs.addButton.addEventListener('click', addTodo);
+
+async function addTodo() {
+  const todoValue = refs.input.value.trim();
+
+  if (!todoValue) {
+    Notiflix.Notify.info('Запиши данні у інпут');
+    return;
+  }
+
+  const postObj = {
+    title: todoValue,
+    completed: false,
+  };
+
+  refs.input.value = '';
+
+  const data = await fetch('https://jsonplaceholder.typicode.com/todos', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify(postObj),
+  });
+
+  const todo = await data.json();
+
+  createHTML(todo);
+}
+// Наступне заняття
+// refs.todoDiv.addEventListener('click', onDelete);
+
+// async function onDelete(id) {}
