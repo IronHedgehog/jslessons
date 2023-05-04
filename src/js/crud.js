85801fe578b843bab72ae627bdb5adf67c20cbf4
IronHedgehog <91615835+IronHedgehog@users.noreply.github.com>
2023-05-04 20:38:53 +0300

    CRUD ,UD, JSON_SERVER

--- src/js/crud.js
+++ src/js/crud.js
@@ -14,24 +14,25 @@ const refs = {
 };
 
 async function getTodos() {
-  const data = await fetch(
-    'https://jsonplaceholder.typicode.com/todos?_limit=10'
-  );
+  const data = await fetch('http://localhost:7775/posts');
   const todos = await data.json();
-  console.log(todos);
+
   todos.forEach(todo => createHTML(todo));
 }
 
 window.addEventListener('DOMContentLoaded', getTodos());
 
 function createHTML({ completed, id, title }) {
-  console.log(completed);
-  const markup = `<div class="form-check" id="todo-${id}">
+  const markup = `<div class="form-check" id="${id}">
   <label class="form-check-label">
-    <input type="checkbox" class="form-check-input" checked="${completed}"/>
+    <input id="${id}" type="checkbox" class="form-check-input" ${
+    completed === true ? 'checked' : null
+    // completed && "checked"
+  }/>
    ${title}
   </label>
   <button
+    id="${id}"
     type="button"
     class="btn-close"
     aria-label="Close"
@@ -58,7 +59,7 @@ async function addTodo() {
 
   refs.input.value = '';
 
-  const data = await fetch('https://jsonplaceholder.typicode.com/todos', {
+  const data = await fetch('http://localhost:7775/posts', {
     method: 'POST',
     headers: {
       'content-type': 'application/json',
@@ -70,7 +71,44 @@ async function addTodo() {
 
   createHTML(todo);
 }
-// Наступне заняття
-// refs.todoDiv.addEventListener('click', onDelete);
 
-// async function onDelete(id) {}
+refs.todoDiv.addEventListener('click', onDelete);
+
+async function onDelete(e) {
+  if (e.target.nodeName !== 'BUTTON') return;
+  const deletedId = e.target.getAttribute('id');
+  const data = await fetch(`http://localhost:7775/posts/${deletedId}`, {
+    method: 'DELETE',
+    headers: {
+      'content-type': 'application/json',
+    },
+  });
+
+  const res = await data.json();
+
+  if (res) {
+    document.getElementById(`${deletedId}`).remove();
+  }
+}
+
+refs.todoDiv.addEventListener('change', onUpdate);
+
+async function onUpdate(e) {
+  const updatedId = e.target.getAttribute('id');
+
+  const completed = document.querySelector(`input`).checked;
+  console.log('object :>> ', completed);
+
+  const data = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${updatedId}`,
+    {
+      method: 'PATCH',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({ completed }),
+    }
+  );
+
+  const res = await data.json();
+}
