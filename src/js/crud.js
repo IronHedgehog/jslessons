63f4a8f5a9c19c97c55e914095b65547839a9426
Artem Faust <artemfaust@MacBook-Air-Artem.local>
2023-05-16 12:29:56 +0300

    test

--- src/js/crud.js
+++ src/js/crud.js
@@ -5,6 +5,7 @@
 // U - update - оновити данні - Якщо оновити об'єкт повністю то PUT, якщо оновити точечно (одне поле) то PATCH
 // D - delete - Видалити - метод DELETE
 
+//test
 const refs = {
   form: document.querySelector('#form'),
   phoneList: document.querySelector('#todos'),
