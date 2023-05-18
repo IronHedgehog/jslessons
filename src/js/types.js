277c8a2e5663e2417c358aaa2a9be4dab0e9655c
Artem Faust <artemfaust@MacBook-Air-Artem.local>
2023-05-18 20:44:42 +0300

    func

--- src/js/types.js
+++ src/js/types.js
@@ -1,11 +1,20 @@
+funct();
+
 const num = 7;
 const num1 = 7;
 
-console.log(num === num1);
+// console.log(num === num1);
 
 const person = {
   name: 'Artem',
   phone: 72324723742,
+  bane: {
+    lon: 2934,
+    lat: {
+      lat: 123,
+      lon: 928,
+    },
+  },
 };
 
 const person1 = {
@@ -13,11 +22,27 @@ const person1 = {
   phone: 72324723742,
 };
 
+const strPr = JSON.stringify(person);
+const person3 = JSON.parse(strPr);
+
+person3.name = 'Petro';
+
+console.log(person);
+console.log(person3);
+
+const person2 = { ...person, ...person1 };
+console.log(person2);
+
+const strPerson = JSON.stringify(person);
+const strPerson1 = JSON.stringify(person1);
+
+// console.log(strPerson === strPerson1);
+
 let a;
-console.log(a);
+// console.log(a);
 a = 10;
 
-console.log(a);
+// console.log(a);
 
 const b = '2';
 
@@ -29,8 +54,8 @@ console.log(b === c);
 
 function culcPlus(n) {
   return function op(num) {
-    console.log(n, num);
-    console.log(n + num);
+    // console.log(n, num);
+    // console.log(n + num);
   };
 }
 
@@ -49,6 +74,52 @@ function makeQrCodes(domen) {
 const dotCom = makeQrCodes('com');
 const dotUa = makeQrCodes('ua');
 
-console.log(dotCom('google'));
+// console.log(dotCom('google'));
 
-console.log(dotUa('robota'));
+// console.log(dotUa('robota'));
+
+const func = (...arguments) => {
+  console.log(arguments);
+  return a + b;
+};
+
+func(1, 2);
+
+function funct(a, b, c) {
+  console.log(arguments);
+  const argumentsArr = Array.from(arguments);
+  console.log(argumentsArr);
+  console.log(a + b + c);
+}
+
+funct(1, 2, 3);
+
+// console.log(nummm);
+const nummm = {};
+
+const arr = [1, 3, 2, 5, 4];
+const arr1 = [6, 7, 8, 9, 10];
+
+const mapArr = arr.map((num, index, arr) => {
+  console.log(num, index, arr);
+});
+
+console.log(mapArr);
+
+const FilterArr = arr.filter(num => num >= 3);
+
+console.log(FilterArr);
+
+const sortArr = arr.sort((a, b) => b - a);
+
+console.log(sortArr);
+
+const arr3 = arr.concat(arr1);
+console.log(arr3);
+
+const arr4 = [...arr, ...arr1];
+arr4.push(11);
+console.log(arr4);
+
+// console.log(typeof null);
+// console.log(Array.isArray(func));
